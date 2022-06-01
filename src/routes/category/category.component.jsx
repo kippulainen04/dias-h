b61bffb2b0e6005d0aa4c4c4766fb68cd36d14fd
Nhan Phan <kippulainenko@gmail.com>
2022-06-01 13:40:01 +0300

    add more responsiveness

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -12,6 +12,11 @@ display: grid;
 grid-template-columns: repeat(4, 1fr);
 column-gap: 20px;
 row-gap: 50px;
+@media screen and (max-width: 800px)
+{
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+} 
 `
 
 const Category = () => {
