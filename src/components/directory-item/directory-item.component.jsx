612b1f5b6974b03b173953742b4fc79e99a23388
Nhan Phan <kippulainenko@gmail.com>
2022-06-06 14:32:49 +0300

    refactor home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,8 +1,67 @@
+import { Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
+import styled from "styled-components"
+
+export const BackgroundImage = styled.div`
+width: 100%;
+height: 100%;
+background-size: cover;
+background-position: center;
+background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+`
+
+export const Body = styled.div`
+width: 120px;
+height: 100px;
+padding: 0 10px;
+opacity: 0.7;
+position: absolute;
+color: #4a4a4a;
+font-weight: 900;
+text-transform: uppercase;
+margin: 0 6px 0;
+display: none;
+text-shadow: '2px 2px 4px'
+`
+
+export const DirectoryItemContainer = styled.div` 
+min-width: 32%;
+height: 450px;
+flex: 1 1 auto;
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+overflow: hidden;
+
+&:hover {
+  cursor: pointer;
+
+  & ${BackgroundImage} {
+    transform: scale(1.1);
+    transition: transform 2s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+  }
+
+  & ${Body} {
+    opacity: 0.9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+@media screen and (max-width: 800px)
+{
+    height: 400px;
+    min-width: 60%;
+}
+`
+
 
 const DirectoryItem = ({category}) => {
-    const {imageUrl, title, route} = category;
+    const {imageUrl , route, title} = category;
     const navigate = useNavigate();
 
     const onNavigateHandler = () => navigate(route);
@@ -13,8 +72,7 @@ const DirectoryItem = ({category}) => {
             imageUrl={imageUrl} 
           />
           <Body>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
+            <Typography variant="h5">{title}</Typography>
           </Body>
         </DirectoryItemContainer>
       </>
--- src/mock-Data.js
+++ src/mock-Data.js
@@ -1,33 +1,32 @@
 export const mockData = [
   {
     id: 1,
-    title: 'reebok',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    route: "shop/reebok"
+    title: 'converse',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dais-h.appspot.com/o/converse.png?alt=media&token=b2a38916-3e32-4954-83a4-3c8628f46847',
+    route: "shop/converse"
   },
   {
     id: 2,
     title: 'fila',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dais-h.appspot.com/o/pexels-enes-c%CC%A7elik-7748767.jpg?alt=media&token=394a03dd-cae0-410b-92d5-d63c6b775acd',
     route: "shop/fila"
   },
   {
     id: 3,
-    title: 'converse',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    route: "shop/converse"
-
+    title: 'reebok',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dais-h.appspot.com/o/reebok.png?alt=media&token=3c309257-78ea-4520-a7f0-753b7c134fab',
+    route: "shop/reebok"
   },
   {
     id: 4,
     title: 'adidas',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dais-h.appspot.com/o/adidas.png?alt=media&token=35d82b0e-4b82-4602-96f2-eb20ab5481bd',
     route: "shop/adidas"
   },
   {
     id: 5,
     title: 'nike',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dais-h.appspot.com/o/nike.png?alt=media&token=a4bfa8ce-e657-467c-8eef-6d202ec4f6f9',
     route: "shop/nike"
   },
 ];
